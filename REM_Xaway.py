3e3823b8cd386e6c72d13ae22514ff63403621cc
Radhika Bhuckory <bhuckory@usc.edu>
2020-06-02 17:12:04 -0700

    fixed plot in main + added code comments

--- REM_Xaway.py
+++ REM_Xaway.py
@@ -1,4 +1,3 @@
-import time
 import math
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
@@ -21,21 +20,19 @@ V_X = 1 #horizontal velocity 1m/s
 X_AWAY = 4 #a distance X meters away from source 
 
 def terminal_velocity(d):
-    ''' This function estimates the terminal velocity (units um/s ?) of a respitory droplet as a function of
-    the droplet's diamter "d" and horizontal velocity "v_x"
+    ''' This function estimates the terminal velocity in m/s of a respitory droplet as a function of
+    the droplet's diamter "d" in micro meters. The terminal velocity also depends on defined constants and variables,
+    gravity, drad coefficient, Reynolds number, and the density of the droplet and air.
 
     Parameters:
         d (float): A float representing the the diamter of the droplet which will be used to calculate the 
-                Reynolds number and then used to find the drag coefficient "CD"
-        v_x (float): In this model, the horizontal velocity of the droplet is equivalent to the velocity of the 
-                of the surrounding air due to the small size of the droplet. In most cases, v_x is assumed 
-                to be 1 m/s.
+                Reynolds number and then used to find the drag coefficient "Cd"
 
     Returns: 
-        v_t (float): This function returns the terminal velocity of the droplet, v_t, when Fdrag = Fgrav
+        v_t (float): v_t, a float value that represents the terminal velocity of the droplet, when Fdrag = Fgrav 
+                    and neglects the Buoyant force. 
 
     '''
-
     reynolds_p = RHO_A*V_X*d/VISCOSITY #reynolds number calculation
     drag_coef = 24*(1+0.15*reynolds_p**0.687)/reynolds_p #Drag Coefficient
     v_t = math.sqrt((4*d*(RHO_D - RHO_A)*G)/3*RHO_A*drag_coef)
@@ -43,20 +40,15 @@ def terminal_velocity(d):
 
 
 def droplet_diameter(time): 
-    ''' This function estimates the droplet's diameter in meters, as a function of time (s), the relative hummidity (RH),
-    and the temperature, T in Kelvin.
+    ''' This function estimates the droplet's diameter in micrometers as a function of time (s) that also depends
+     on the relative hummidity (RH) and the temperature (T) in Kelvin which are set as constants in this program.
 
     Parameters:
-        time (float): This parameter represents what time the dimater of the droplet will be calclulated, since
-                    the size of the droplet evaporates over time.
-        r_h (int): An integer repsenting the relative hummidity of the ambient conditions. 
-        temp (float): This parameter represents the temperature of the ambient conditions in Kelvin which will be used
-                     in the evaporation rate, beta to solve for the diameter. 
-
+        time (float): This parameter represents the time at which the diameter of the droplet will be calclulated, 
+                    since the size of the droplet evaporates over time.
     Returns:
-        d (float): This function returns d, a value representing the diameter of the droplet after it has 
+        d (float): Returns d, a float value representing the diameter of the droplet after it has 
                 evaporated after t seconds. 
-
     '''
     molec_diff = (2.16*10**-5)*(TEMPERATURE/273.15)**1.8 #molecular diffusivity of water vapor
     p_sat = 611.21**(((19.843-TEMPERATURE)/234.5)*((TEMPERATURE-273.15)/(TEMPERATURE-16.01)))
@@ -68,21 +60,20 @@ def droplet_diameter(time):
 
     return d
 
-def position(time): # function that returns x and z position as tuple
-    ''' This function estimates the horizontal distance the droplet has travelled at a certain terminal velocity and
-    horizontal velocity.  
+def position(time): 
+    ''' This function estimates the horizontal and vertical distance the droplet has travelled at an inputted time for a 
+    certain terminal velocity, horizontal velocity, initital X_0 and Z_0 
 
     Parameters:
-        v_x (float): In this model, the horizontal velocity of the droplet is equivalent to the velocity of the 
-                of the surrounding air due to the small size of the droplet. In most cases, v_x is assumed 
-                to be 1 m/s.
-        v_t (float): A float that corresponds to the terminal velocity of the droplet
+        time (float): Time in seconds at which the x_d and z_d values are evaluated.
 
     Returns: 
-        x_d (float): returns the horizontal distance of the drop in meters.  
-    '''	
+        (x_d,z_d): a 2-tuple of float values containing the horizontal and vertical distance of the drop in meters
+                   after t seconds.
+    '''
     d = droplet_diameter(time)
     v_t = terminal_velocity(d)
+
     time_to_hit_ground = Z_0/v_t
     x_d = X_0 + V_X*min(time, time_to_hit_ground)
     z_d = max(Z_0-v_t*time,0) #take into account droplet hitting the ground
@@ -91,6 +82,16 @@ def position(time): # function that returns x and z position as tuple
     return distance_tuple
 
 def concentration(time,x_away=X_AWAY): 
+    ''' This function estimates the concentration of the droplets by modeling each breath as an ever expanding 
+    Gaussian distribution.
+
+    Parameters:
+        time (float): Time in seconds at which the Gaussian distribution is evaluated.
+
+    Returns: 
+        conc_of_puff (float): a float value containing the concentration of the puff that interacts with a person X_AWAY 
+        from the infected source.
+    ''' 
     distance_tuple = position(time)
     sigma = A*(x_away**B)
     x_d = distance_tuple[0]
@@ -99,18 +100,38 @@ def concentration(time,x_away=X_AWAY):
     return conc_of_puff
 
 def exposure_per_breath(time,x_away=X_AWAY): 
+    ''' This function estimates the dose of respiratory droplets that a person is exposed to by integrating the concentration 
+    function above over the duration that the puff travels in the air. This function uses the python library scipy and the quad 
+    function to evaluate the integral. 
+
+    Parameters:
+        time (float): Time in seconds that represents the upper limit at which the integral is evaluated.
+
+    Returns: 
+        exposure (2-tuple float): A 2-tuple of float value containing the concentration of the puff integrated over the inputted 
+        time, and the error due to possible numerical error in the integrand from the use of quad 
+    ''' 
     exposure = integrate.quad(concentration, 0, time, args=(x_away,)) #keep x_away constant while integrating
-    #print(exposure)
     return exposure
 
 def total_exposure(time=5,x_away=X_AWAY):
+    ''' This function estimates the total dosage a person is exposed to after many Guassian puffs by multiplying the
+    respiratory rate x time to find the number of times the infected person has exhaled droplets into the air by exposure_per_breath
+    function. 
+
+    Parameters:
+        time (float): Time in seconds for which the the total exposure is calculated.
+
+    Returns: 
+        total_dosage (float): a float value representing the total dosage a person is exposed to after several breaths are
+        taken from an infected source. 
+    ''' 
     exposure_tuple = exposure_per_breath(time,x_away)
     number_of_breaths = RESPIRATORY_RATE*time
     total_dosage = exposure_tuple[0]*number_of_breaths
     #print(total_dosage)
     return total_dosage
     
-
 
 if __name__ == '__main__':
     #exposure_over_distance(50,4)
@@ -133,5 +154,3 @@ if __name__ == '__main__':
     plt.title('Concentration vs Droplet Size Graph')
     #plt.figure()
     plt.show()
-
-
