6c8b026ec8a3dc49a09cac1c4dbc0ef111fe467b
Radhika Bhuckory <bhuckory@usc.edu>
2020-06-16 12:46:05 -0700

    dispersion coeff bug

--- REM.py
+++ REM.py
@@ -101,10 +101,10 @@ def concentration(time):
         conc_of_puff (float): a float value containing the concentration of the puff that interacts with a person X_AWAY 
         from the infected source.
     '''	
-    distance_tuple = position(time)
-    sigma = A*(X_AWAY**B) #dispersion coefficient 
+    distance_tuple = position(time) 
     x_d = distance_tuple[0]
     z_d = distance_tuple[1]
+    sigma = A*(x_d**B) #dispersion coefficient
     conc_of_puff = (NUMBER_OF_DROPLETS/((math.sqrt(2*math.pi)*sigma))**3)*math.exp((-1/(2*sigma**2))*((X_AWAY-x_d)**2+z_d**2))
     return conc_of_puff
 
